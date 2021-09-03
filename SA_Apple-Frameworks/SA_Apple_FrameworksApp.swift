ea70c33631e445ddd69fce54f484fd16af33260a
Alexander <hello@alexander.works>
2021-09-04 01:09:29 +0300

    Initial Commit

---
+++ SA_Apple-Frameworks/SA_Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SA_Apple_FrameworksApp.swift
+//  SA_Apple-Frameworks
+//
+//  Created by Alexander on 04.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SA_Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
